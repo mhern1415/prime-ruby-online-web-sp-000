551ff341d8e43bec5cc403f535edb9cb3b79beea
Mark Hernandez <m.hern1415@gmail.com>
2019-05-06 18:35:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  range = (2..(number - 1)).to_a
+  check = true
+  if number < 2
+    check = false
+  else
+    range.each do |x|
+      if number % x == 0
+        check = false
+        break
+      end
+    end
+  end
+   puts check
+end
